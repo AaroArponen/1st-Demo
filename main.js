a4cebf03e01d868eb7b12a9ddfeaf5fdffab579c
Aaro Arponen <aaro.arponen@gmail.com>
2018-09-18 22:53:29 +0300

    Renderöitiin musta gl-näkymä.

---
+++ main.js
@@ -0,0 +1,15 @@
+main();
+
+
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    const gl = canvas.getContext("webgl");
+    
+    if (gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    gl.clearColor (0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
