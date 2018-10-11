0d8b2fa872570e764c57116224d8c05a8f779969
Aaro Arponen <aaro.arponen@gmail.com>
2018-10-11 22:37:26 +0300

    Variablet createShadereille ja createProgram

--- main.js
+++ main.js
@@ -10,8 +10,35 @@ function main() {
         return;
     }
 
-    gl.clearColor (0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    var vertexShaderSource = document.getElementById("2d-vertex-shader");
+    var fragmentShaderSource = document.getElementById("2d-fragment-shader");
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    var program = createProgram(gl, vertexShader, fragmentShader);
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+    var positions = [
+        0.4, 0.1,
+        1.0, 0.4,
+        0.9, 0.8,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+}
+
+function createProgram(gl, vertexShader, fragmentShader){
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success){
+        return program;
+    }
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
 }
 
 function createShader (gl, type, source) {
@@ -24,5 +51,4 @@ function createShader (gl, type, source) {
     }
     console.log(gl.GetShaderInfoLog)(shader));
     gl.deleteShader(shader);
-
 }
